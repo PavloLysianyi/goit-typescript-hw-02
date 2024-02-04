c0d861386b35201d15c39b7fc8f32ddd8c746411
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2024-02-04 23:21:31 +0200

    do task

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,19 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
+
+const stringKeyValuePair: KeyValuePair<string, number> = {
+  key: "example",
+  value: 42,
+};
+
+const numberKeyValuePair: KeyValuePair<number, string> = {
+  key: 123,
+  value: "example",
+};
 
 export {};
